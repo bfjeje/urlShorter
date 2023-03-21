91cfee12f904260ab110f0002b7cd4108db9d098
Bruno Ellerbach <ingbrunofernandez@gmail.com>
2023-03-21 19:42:00 -0300

    Created a new API

--- README.md
+++ README.md
@@ -2,14 +2,18 @@
 A simple URL shorter made with Node.JS, Express and MariaDB
 
 # Steps:
-1) npm install
-2) node index.js
+1) No banco de dados, criar o banco de dados chamado urlshorter, e dentro dele executar o SQL que esta no arquivo create_table.sql
+2) npm install nodemon
+2) npm run dev
+3) 
 
 # API Info:
+### Ponto 1: um método de encurtar uma URL persistindo-a no banco de dados
 [POST] http://localhost:3001/urlshorter/encurtar - Params: {<br />
 "urlLonga": "http://www.facebook.com/teste4" <br />
 }<br />
 Retorno: {"id": 10}<br /><br />
+### Ponto 2: um método que retorna uma url encurtada conforme um id
 [GET] http://localhost:3001/urlshorter/:id - <br />
 Retorno: [<br />
 { <br />
@@ -19,6 +23,7 @@ Retorno: [<br />
 "created_at": "2023-03-23T02:02:17.000Z" <br />
 } <br />
 ]<br /> <br />
+### Ponto 3: um método que retorna todas as URLs encurtadas em uma data específica
 [POST] http://localhost:3001/urlshorter/all - Params: {<br />
 "dateTime": "2023-03-20" <br />
 }<br />
@@ -30,3 +35,12 @@ Retorno: [ <br />
 "urlshort": "http://short.by/rgl29p" <br />
 } <br />
 ]<br /><br />
+### Ponto : um método que retorna uma url encurtada conforme o encurtamento da URL
+[POST] http://localhost:3001/urlshorter/urlshort - Params: {<br />
+"encurtamento": "bul344" <br />
+}<br />
+Retorno: [ <br />
+{ <br />
+"urlshort": "http://short.by/bul344" <br />
+} <br />
+]
--- routes/urlShorter.js
+++ routes/urlShorter.js
@@ -2,29 +2,9 @@ const express = require('express');
 const pool = require('../helpers/database')
 const router = express.Router();
 
-router.get('/:id', async function (req, res) {
-    try {
-        const sqlQuery = 'SELECT * FROM url WHERE id=?'
-        const rows = await pool.query(sqlQuery, req.params.id);
-        res.status(200).json(rows);
-    }catch (error){
-        res.status(400).send(error.message)
-    }
-});
+const URL_START = "http://short.by/";
 
-router.post('/all', async function (req, res) {
-    const {dateTime} = req.body;
-
-    try {
-        const sqlQuery =
-            "SELECT DISTINCT urlshort FROM url WHERE DATE_FORMAT(created_at, '%Y-%m-%d') = ?"
-        const rows = await pool.query(sqlQuery, dateTime);
-        res.status(200).json(rows);
-    }catch (error){
-        res.status(400).send(error.message)
-    }
-});
-
+/***Ponto 1: Um método de encurtar uma URL persistindo-a no banco de dados.***/
 router.post('/encurtar', async function (req, res){
     try {
         const {urlLonga} = req.body;
@@ -40,8 +20,48 @@ router.post('/encurtar', async function (req, res){
     }
 })
 
+/***Ponto 2: um método que retorna uma url encurtada conforme um id.***/
+router.get('/:id', async function (req, res) {
+    try {
+        const sqlQuery = 'SELECT * FROM url WHERE id=?'
+        const rows = await pool.query(sqlQuery, req.params.id);
+        res.status(200).json(rows);
+    }catch (error){
+        res.status(400).send(error.message)
+    }
+});
+
+/***Ponto 3: um método que retorna todas as URLs encurtadas em uma data específica.***/
+router.post('/all', async function (req, res) {
+    const {dateTime} = req.body;
+
+    try {
+        const sqlQuery =
+            "SELECT DISTINCT urlshort FROM url WHERE DATE_FORMAT(created_at, '%Y-%m-%d') = ?"
+        const rows = await pool.query(sqlQuery, dateTime);
+        res.status(200).json(rows);
+    }catch (error){
+        res.status(400).send(error.message)
+    }
+});
+
+/***Ponto 4: um método que retorna uma url encurtada conforme o encurtamento da URL.***/
+router.post('/shorturl', async function (req, res) {
+    const {encurtamento} = req.body;
+
+    try {
+        const sqlQuery =
+            "SELECT DISTINCT urlshort FROM url WHERE urlshort = ?"
+        const rows = await pool.query(sqlQuery, URL_START+encurtamento);
+        res.status(200).json(rows);
+    }catch (error){
+        res.status(400).send(error.message)
+    }
+});
+
 function getRandomURL() {
-    return "http://short.by/"+Math.random().toString(32).substring(2, 5) + Math.random().toString(32).substring(2, 5);
+
+    return URL_START+Math.random().toString(32).substring(2, 5) + Math.random().toString(32).substring(2, 5);
 }
 
 module.exports = router;
