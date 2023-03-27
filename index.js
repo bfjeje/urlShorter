74fa167855d31d9bc1f961fb0aada39614a17a38
Bruno Ellerbach <ingbrunofernandez@gmail.com>
2023-03-27 18:58:40 -0300

    Added documentation

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const YAML = require('yamljs');
+const swaggerUi = require('swagger-ui-express');
 
 dotenv.config({path: '.env-local'});
 
-//I get the port from file .env-local, or default port: 3000.
-const PORT = process.env.PORT || '3001';
+//I get the port from file .env-local, or default port: 3001.
+const PORT = process.env.PORT || '3000';
 
 const app = express();
+
+const spec = YAML.load("./docs/openAPI.yaml");
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(spec));
 
 /*
 Middleware
